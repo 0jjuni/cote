fa6f67509986a5b2e05af50b6439a313fec99aca
0jjuni <133225625+0jjuni@users.noreply.github.com>
2025-02-12 17:54:08 +0900

    Time: 219 ms (5.14%), Space: 18.7 MB (30.54%) - LeetHub

--- LeetCode/0125-valid-palindrome/0125-valid-palindrome.py
+++ LeetCode/0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,14 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        s_list = [i for i in s if i.isalpha()| i.isdigit()]
+        s_list = [i for i in s if i.isalnum()]
 
-        while True:
-            if len(s_list) <= 1:
-                return True
+        while len(s_list) > 1:
             first = s_list.pop(0)
             last = s_list.pop()
             if first == last:
                 continue
             else:
                 return False
+
+        return True
