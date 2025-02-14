9182411065aaf48fb59ce0512fb357ed490530a3
0jjuni <133225625+0jjuni@users.noreply.github.com>
2025-02-14 19:17:09 +0900

    Time: 7 ms (98.18%), Space: 20.7 MB (76.36%) - LeetHub

--- LeetCode/0049-group-anagrams/0049-group-anagrams.py
+++ LeetCode/0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         strs_dic= collections.defaultdict(list)
-        for idx, word in enumerate(strs):
-            sorted_word = ''.join(sorted(word))
-            
-            if strs_dic[sorted_word]:
-                strs_dic[sorted_word].append(word)
-            else:
-                strs_dic[sorted_word] = [word]
+        
+        for word in strs:
+            strs_dic[''.join(sorted(word))].append(word)
         
         return list(strs_dic.values())
