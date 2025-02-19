fda01593c7838edb2a15cd17453de190433176ed
0jjuni <133225625+0jjuni@users.noreply.github.com>
2025-02-19 15:57:58 +0900

    Time: 0 ms (100%), Space: 17.9 MB (56.61%) - LeetHub

--- LeetCode/0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ LeetCode/0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -9,23 +9,23 @@ class Solution:
         left_before, rev_finish= None, None
         rev = None
 
-        if left == 1:
+        if left == 1: # 처음부터 역순 시작할 때
             left_before = head
             rev_finish = head            
         else:
-            for _ in range(left-2):
+            for _ in range(left-2): # 역순되지 않는 값들의 마지막값을 저장하기 위해 -2만큼 반복
                 cur = cur.next
-            left_before = cur
+            left_before = cur # 역순되지 않은 값들 중 마지막 값
             cur = cur.next
-            rev_finish = cur
+            rev_finish = cur # 역순될 값들 중 마지막에 올 값
 
-        for i in range(right-left+1):
+        for i in range(right-left+1): # 역순
             rev, rev.next, cur = cur, rev, cur.next
 
-        if cur:
+        if cur: # right뒤쪽에 남은게 있다면 rev 뒤에 추가
             rev_finish.next = cur
 
-        if left == 1:
+        if left == 1: # 처음부터 시작했으므로 rev를 return
             head = rev
         else:
             left_before.next = rev
