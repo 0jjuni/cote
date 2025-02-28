1c862a22193928feb9543a9f53933064b75970b5
0jjuni <133225625+0jjuni@users.noreply.github.com>
2025-02-28 11:12:25 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
@@ -5,11 +5,10 @@ N = int(sys.stdin.readline())
 result = 0
 
 for A in range(1, N+1):
-    for B in range(1, N+1):
+    for B in range(A+2, N+1):
         C = N - A - B
         if A + B + C == N:
-            if A >= B + 2:
-                if C % 2 == 0 and C > 0:
-                    result +=1
+            if C % 2 == 0 and C > 0:
+                result +=1
 
 print(result)
