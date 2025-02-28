afad68530217f67df5191192cbd55b5b0400e302
0jjuni <133225625+0jjuni@users.noreply.github.com>
2025-02-28 11:10:10 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
@@ -6,10 +6,10 @@ result = 0
 
 for A in range(1, N+1):
     for B in range(1, N+1):
-        for C in range(1, N+1):
-            if A + B + C == N:
-                if A >= B + 2:
-                    if C % 2 == 0:
-                        result +=1
+        C = N - A - B
+        if A + B + C == N:
+            if A >= B + 2:
+                if C % 2 == 0 and C > 0:
+                    result +=1
 
 print(result)
