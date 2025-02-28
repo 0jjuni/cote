4dc6b094c3d2aaabfd9ca1b2182218db709aa315
0jjuni <133225625+0jjuni@users.noreply.github.com>
2025-02-28 11:23:53 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 452 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,12 +1,14 @@
 import sys
 
 num_list = list(map(int, sys.stdin.readline().split()))
-result = 0
 
-for x in range(-999,1000):
-    for y in range(-999,1000):
-        if num_list[0] * x + num_list[1] * y == num_list[2] and num_list[3] * x + num_list[4] * y == num_list[5]:
-            print(x, y)
-            break
+def get_sol(num_list):
+    for x in range(-999,1000):
+        for y in range(-999,1000):
+            if num_list[0] * x + num_list[1] * y == num_list[2] and num_list[3] * x + num_list[4] * y == num_list[5]:
+                print(x, y)
+                return
+get_sol(num_list)            
+
     
 
